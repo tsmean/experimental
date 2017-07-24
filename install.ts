3c18da8f9f8c2c1f14c12c5f62a33ffdabd9aace
bersling <bersling@gmail.com>
2017-07-24 09:06:15 +0200

    fix

--- install.ts
+++ install.ts
@@ -1,10 +1,23 @@
 import * as process from 'process';
-import { spawn } from 'child_process';
+import {spawnSync, SpawnSyncOptionsWithStringEncoding, SpawnSyncReturns} from 'child_process';
+
+// declaring utf8 makes sure a string is returned (as opposed to a buffer) from spawnSync
+const spawnSyncOptions: SpawnSyncOptionsWithStringEncoding = {
+  encoding: 'utf8'
+};
 
 const directoriesToSetUp = [
   'backend',
   'frontend'
 ];
+
+/**
+ * Some few adaptions are necessary for making installation work on windows
+ */
+const isWin = /^win/.test(process.platform);
+const spawnSyncCommand = (cmdName): string => {
+  return isWin ? `${cmdName}.cmd` : cmdName;
+};
 
 const startingDirectory = process.cwd();
 
@@ -17,6 +30,7 @@ directoriesToSetUp.forEach(dirName => {
 
 function changeToDirectory (dir) {
   try {
+    console.log(`Changing to directory ${dir}`);
     process.chdir(dir);
   } catch (err) {
     console.log(`Could not change to directory ${dir}: ${err}`);
@@ -25,19 +39,29 @@ function changeToDirectory (dir) {
 }
 
 function runInstallScript () {
-  const child = spawn('npm', ['run', 'install']);
+  const child = spawnSync(spawnSyncCommand('npm'), ['install'], spawnSyncOptions);
+  handleCommandResult(child, {exitOnError: false});
+}
 
-  child.stdout.setEncoding('utf8').on('data', (chunk) => {
-    console.log(chunk);
-  });
+/**
+ * Helper functions
+ */
+function handleCommandResult(result: SpawnSyncReturns<string>, options: HandleCommandResultOptions) {
+  if (result.error) {
+    console.error('ERROR IN PROCESS:', result.error);
+    if (options.exitOnError) {
+      process.exit()
+    }
+  } else if (result.stderr !== undefined && result.stderr !== "") {
+    console.error('STDERR NOT EMPTY:', result.stderr);
+    if (options.exitOnError) {
+      process.exit()
+    }
+  } else {
+    console.log(result.stdout);
+  }
+}
 
-  child.stderr.setEncoding('utf8').on('data', (chunk) => {
-    console.error(chunk);
-    process.exit();
-  });
-
-  child.on('close', (code) => {
-    console.log(`Child process exited with code ${code}`);
-  });
-
+interface HandleCommandResultOptions {
+  exitOnError: boolean
 }
